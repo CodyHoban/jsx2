3ac4056d50049e5e0a20ff116408194397ad2f58
Cody <hoban.j.cody@gmail.com>
2021-07-22 19:21:23 -0400

    reusable components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45"
+          post="Green man Rules!"
+          avatarImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Frank" 
+          timeAgo="Today at 3:17" 
+          post="Red man Loses!" 
+          avatarImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Today at 9:02" 
+          post="Blue man Wins!" 
+          avatarImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
